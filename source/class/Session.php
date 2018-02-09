4972dd8a0f6164d27c94dd4c1c14e454a806fcd9
Jean luc <you@example.com>
2018-02-09 12:14:36 +0100

    synchro

--- source/class/Session.php
+++ source/class/Session.php
@@ -6,10 +6,39 @@ namespace Phi\Session;
 
 class Session
 {
+    private static $sessionStarted = false;
+    protected $variables;
 
     public function __construct()
     {
+        if(!static::$sessionStarted) {
+            session_start();
+        }
 
+        $this->variables = &$_SESSION;
     }
 
+    public function set($name, $value)
+    {
+        $this->variables[$name] = $value;
+        return $this;
+    }
+
+    public function get($name, $ifNull = null)
+    {
+        if(array_key_exists($name, $this->variables)) {
+            return $this->variables[$name];
+        }
+        else {
+            return $ifNull;
+        }
+    }
+
+    public function delete($name)
+    {
+        if(array_key_exists($name, $this->variables)) {
+            unset($this->variables[$name]);
+        }
+        return $this;
+    }
 }
